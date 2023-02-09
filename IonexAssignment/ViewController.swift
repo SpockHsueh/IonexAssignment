76efefd105036eed9a178d706723f00d574cd62d
Spock <spock@firstory.me>
2023-02-09 23:01:20 +0800

    start

---
+++ IonexAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IonexAssignment
+//
+//  Created by 薛宇振 on 2023/2/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
